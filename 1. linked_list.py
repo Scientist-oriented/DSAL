a86a9dddd95ec263225606b2db8f7743e501e80b
Scientist-oriented <moon910904@gmail.com>
2020-10-15 22:13:05 +0900

    linked list 구현 완료

--- 1. linked_list.py
+++ 1. linked_list.py
@@ -1,28 +1,33 @@
-class node:
+class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
 class nodemngt:
     def __init__(self, head):
-        self.head = head
+        self.head = Node(head)
 
     def add(self, data):
         if self.head == None:
-            self.head = data
+            self.head = Node(data)
         else:
             node = self.head
             while node.next:
                 node = node.next
-            node.next = data
+            node.next = Node(data)
 
     def display(self):
         node = self.head
         while node:
-            print(node)
+            print(node.data)
             node = node.next 
 
 
 test_list = nodemngt(0)
+test_list.display()
+
+for data in range(1, 10):
+    test_list.add(data)
+
 test_list.display()
 
