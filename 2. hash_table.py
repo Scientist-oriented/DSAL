275c6f909118b893710ddd40711b2cff5f0142e5
Scientist-oriented <moon910904@gmail.com>
2020-10-17 17:21:05 +0900

    2. hash_table 추가

---
+++ 2. hash_table.py
@@ -0,0 +1,28 @@
+# 파이썬으로 해쉬함수를 갖춘 해쉬테이블 구현하기
+
+hash_table = [0 for i in range(10)]
+print(hash_table)
+
+def hash_func(key):
+    return key % 10
+
+def hash_key(data):
+    return ord(data[0])
+
+def store_data(data, value):
+    key = hash_func(hash_key(data))
+    hash_table[key] = value
+
+def get_data(data):
+    key = hash_func(hash_key(data))
+    print(hash_table[key])
+
+store_data("Andy", "English")
+store_data("Dave", "Math")
+store_data("Cathy", "Korean")
+
+print(hash_table)
+
+get_data("Andy")
+get_data("Dave")
+get_data("Cathy")
