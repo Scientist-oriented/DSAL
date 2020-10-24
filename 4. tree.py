f722910e282ea1aab1a9cb8b8b5409e511d0496c
Scientist-oriented <moon910904@gmail.com>
2020-10-24 19:07:01 +0900

    트리 자료구조 delete기능 구현

--- 4. tree.py
+++ 4. tree.py
@@ -30,7 +30,7 @@ class NodeCon:
         node = self.head
         while node:
             if node.value == value:
-                print(value, " Found in the Tree")
+                #print(value, " Found in the Tree")
                 return
             elif node.value > value:
                 node = node.left
@@ -56,7 +56,7 @@ class NodeCon:
                 parent_node = node
                 node = node.right
 
-        if searched = False:
+        if searched == False:
             print("No data Cannot delete")
             return False
 
@@ -71,7 +71,14 @@ class NodeCon:
 
         # 삭제할 node가 child를 하나 가지고 있을 경우
 
-        elif node.left == None or node.right == None:
+        elif node.left != None and node.right == None:
+            if value < parent_node.value:
+                parent_node.left = node.left
+            else:
+                parent_node.right = node.left
+            del node
+
+        elif node.left == None and node.right != None:
             if value < parent_node.value:
                 parent_node.left = node.right
             else:
@@ -87,10 +94,25 @@ class NodeCon:
                 while sub_node.left:
                     sub_node_parent = sub_node
                     sub_node = sub_node.left
-                parent_node.left = 
-
+                sub_node_parent.left = sub_node.right
+                parent_node.left = sub_node
+                sub_node.left = node.left
+                sub_node.right = node.right
+                del node
+            else:
+                sub_node = node.right
+                sub_node_parent = node.right
+                while sub_node.left:
+                    sub_node_parent = sub_node
+                    sub_node = sub_node.left
+                sub_node_parent.left = sub_node.right
+                parent_node.right = sub_node
+                sub_node.left = node.left
+                sub_node.right = node.right
+                del node
                 
-
+        print("Data deleted")
+        return
 
 tree_head = Node(1)
 tree = NodeCon(tree_head)
@@ -106,3 +128,37 @@ tree.search(2)
 tree.search(3)
 tree.search(10)
 tree.search(20)
+
+print("-------------delete test----------------")
+
+import random
+
+test_nums = set()
+while len(test_nums) != 100:
+    test_nums.add(random.randint(0, 999))
+
+print(test_nums)
+
+test_tree_head = Node(500)
+test_tree = NodeCon(test_tree_head)
+
+for nums in test_nums:
+    test_tree.insert(nums)
+
+for nums in test_nums:
+    test_tree.search(nums)
+
+print("test ready")
+
+test_delete_nums = set()
+test_nums_list = list(test_nums)
+    # set는 인덱싱이 불가능하다!
+
+while len(test_delete_nums) != 10:
+    test_delete_nums.add(test_nums_list[random.randint(0, 99)])
+
+print(test_delete_nums)
+
+for nums in test_delete_nums:
+    test_tree.delete(nums)
+
