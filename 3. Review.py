17bc70bafccee22a8e9773f6e7cfe54d4316c99e
Scientist-oriented <moon910904@gmail.com>
2020-11-06 16:07:31 +0900

    이진탐색트리까지 복습

--- 3. Review.py
+++ 3. Review.py
@@ -1,5 +1,5 @@
 # 해쉬테이블
-
+"""
 hash_table = [0 for i in range(10)]
 
 def get_key(data):
@@ -50,21 +50,191 @@ read_data("KIm")
 read_data("Johnson")
 read_data("Yamamoto")
 
-# 버블정렬
-def bubblesort(data):
-    for index in range(len(data)-1):
-        swap = False
-        for index2 in range(len(data) - index - 1):
-            if data[index2] > data[index2 + 1]:
-                data[index2], data[index2 + 1] = data[index2 + 1], data[index2]
-                swap = True
-        if swap == False:
-            break
+# linear probing
+
+hash_table = [0 for i in range(10)]
+
+def get_key(data):
+    return hash(data)
+
+def hash_function(key):
+    return key % 10
+
+def save_data(data, value):
+    key = get_key(data)
+    address = hash_function(key)
+
+    if hash_table[address] != 0:
+        for index in range(address, len(hash_table)):
+            if hash_table[index] == 0:
+                hash_table[index] = [key, value]
+                return
+            elif hash_table[index][0] == key:
+                hash_table[index][1] = value
+                return
+
+    else:
+        hash_table[address] = [key, value]
+
+def read_data(data):
+    key = get_key(data)
+    address = hash_function(key)
+
+    if hash_table[address] != 0:
+        for index in range(address, len(hash_table)):
+            if hash_table[index] == 0:
+                return None
+            elif hash_table[index][0] == key:
+                return hash_table[address][1]
+    else:
+        return None
+
+save_data("KIm", "Korea")
+save_data("Johnson", "USA")
+save_data("Yamamoto", "Japan")
+
+print(hash_table)
+
+a = read_data("KIm")
+b = read_data("Johnson")
+c = read_data("Yamamoto")
+
+print(a)
+print(b)
+print(c)
+
+"""
+
+
+# binary search tree
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class Nodecon:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value):
+        self.current_node = self.head
+
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+
+    def search(self, value):
+        self.current_node = self.head
+
+        while self.current_node:
+            if value == self.current_node.value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+
+        return False
+
+    def delete(self, value):
+        searched = False
+        self.current_node = self.head
+        self.parent_node = self.head
+
+        while self.current_node:
+            if value == self.current_node.value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent_node = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent_node = self.current_node
+                self.current_node = self.current_node.right
+
+        if searched == False:
+            return False
+
+        if self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent_node.value:
+                self.parent_node.left = None
+            else:
+                self.parent_node.right = None
+
+        elif self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent_node.value:
+                self.parent_node.left = self.current_node.left
+            else:
+                self.parent_node.right = self.current_node.left
+
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent_node.value:
+                self.parent_node.left = self.current_node.right
+            else:
+                self.parent_node.right = self.current_node.right
+
+        else:
+            if value < self.parent_node.value:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent_node.left = self.change_node
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right
+
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent_node.right = self.change_node
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right
 
 import random
 
-Testlist = random.sample(range(100), 50)
-print(Testlist)
+bst_nums = set()
+while len(bst_nums) != 100:
+    bst_nums.add(random.randint(0, 999))
 
-bubblesort(Testlist)
-print(Testlist)
+head = Node(500)
+binary_tree = Nodecon(head)
+for num in bst_nums:
+    binary_tree.insert(num)
+    
+for num in bst_nums:
+    if binary_tree.search(num) == False:
+        print ('search failed', num)
+
+delete_nums = set()
+bst_nums = list(bst_nums)
+while len(delete_nums) != 10:
+    delete_nums.add(bst_nums[random.randint(0, 99)])
+
+for del_num in delete_nums:
+    if binary_tree.delete(del_num) == False:
+        print('delete failed', del_num)
+
