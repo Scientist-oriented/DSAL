93fd961b14ae5dc34c7421f3bffc0591d785b06a
Scientist-oriented <moon910904@gmail.com>
2020-11-14 12:24:56 +0900

    힙연습 2회

--- 3. Review.py
+++ 3. Review.py
@@ -1272,4 +1272,208 @@ def max_value(data_list, capacity):
 
 b = max_value(data_list, 30)
 print(b)
+
+
+# 힙 연습
+
+class Heap:
+    def __init__(self, data):
+        self.heap_array = list()
+        self.heap_array.append(None)
+        self.heap_array.append(data)
+
+    def insert(self, data):
+        if len(self.heap_array) == 1:
+            self.heap_array.append(data)
+            return True
+        
+        self.heap_array.append(data)
+        inserted_index = len(self.heap_array) - 1
+
+        while self.move_up(inserted_index):
+            parent_index = inserted_index // 2
+            self.heap_array[inserted_index], self.heap_array[parent_index] = self.heap_array[parent_index], self.heap_array[inserted_index]
+            inserted_index = parent_index
+
+        return True
+
+    def move_up(self, inserted_index):
+        if inserted_index <= 1:
+            return False
+
+        parent_index = inserted_index // 2
+
+        if self.heap_array[parent_index] < self.heap_array[inserted_index]:
+            return True
+        else:
+            return False
+        
+    def pop(self):
+        if len(self.heap_array) <= 1:
+            return False
+        
+        returned_data = self.heap_array[1]
+        self.heap_array[1] = self.heap_array[-1]
+        del self.heap_array[-1]
+        popped_idx = 1
+
+        while self.move_down(popped_idx):
+            left_child_idx = 2 * popped_idx
+            right_child_idx = 2 * popped_idx + 1
+
+            if right_child_idx >= len(self.heap_array):
+                if self.heap_array[popped_idx] < self.heap_array[left_child_idx]:
+                    self.heap_array[popped_idx], self.heap_array[left_child_idx] = self.heap_array[left_child_idx], self.heap_array[popped_idx]
+                    popped_idx = left_child_idx
+            else:
+                if self.heap_array[left_child_idx] > self.heap_array[right_child_idx]:
+                    if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[left_child_idx] = self.heap_array[left_child_idx], self.heap_array[popped_idx]
+                        popped_idx = left_child_idx
+                else:
+                    if self.heap_array[right_child_idx] > self.heap_array[popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[right_child_idx] = self.heap_array[right_child_idx], self.heap_array[popped_idx]
+                        popped_idx = right_child_idx
+
+        return returned_data
+                  
+
+    def move_down(self, popped_idx):
+        left_child_idx = 2 * popped_idx
+        right_child_idx = 2 * popped_idx + 1
+
+        if left_child_idx >= len(self.heap_array):
+            return False
+        elif right_child_idx >= len(self.heap_array):
+            if self.heap_array[popped_idx] < self.heap_array[left_child_idx]:
+                return True
+            else:
+                return False
+        else:
+            if self.heap_array[left_child_idx] > self.heap_array[right_child_idx]:
+                if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                    return True
+                else:
+                    return False
+            else:
+                if self.heap_array[right_child_idx] > self.heap_array[popped_idx]:
+                    return True
+                else:
+                    return False
+
+
+heap = Heap(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
+print(heap.heap_array)
+
+a = heap.pop()
+print(a)
+
+print(heap.heap_array)
+
 """
+
+# 힙 연습
+
+class Heap:
+    def __init__(self, data):
+        self.heap_array = list()
+        self.heap_array.append(None)
+        self.heap_array.append(data)
+
+    def insert(self, data):
+        if len(self.heap_array) == 1:
+            self.heap_array.append(data)
+            return True
+
+        self.heap_array.append(data)
+        inserted_idx = len(self.heap_array) - 1
+
+        while self.move_up(inserted_idx):
+            parent_idx = inserted_idx // 2
+            if self.heap_array[parent_idx] < self.heap_array[inserted_idx]:
+                self.heap_array[parent_idx], self.heap_array[inserted_idx] = self.heap_array[inserted_idx], self.heap_array[parent_idx]
+                inserted_idx = parent_idx
+
+        return True
+
+
+    def move_up(self, inserted_idx):
+        if inserted_idx == 1:
+            return False
+        parent_idx = inserted_idx // 2
+        if self.heap_array[parent_idx] < self.heap_array[inserted_idx]:
+            return True
+        else:
+            return False
+
+    def pop(self):
+        if len(self.heap_array) <= 1:
+            return False
+
+        returned_data = self.heap_array[1]
+        self.heap_array[1] = self.heap_array[-1]
+        del self.heap_array[-1]
+        popped_idx = 1
+
+        while self.move_down(popped_idx):
+            left_child_idx = popped_idx * 2
+            right_child_idx = popped_idx * 2 + 1
+
+            
+            if right_child_idx >= len(self.heap_array):
+                if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                    self.heap_array[left_child_idx], self.heap_array[popped_idx] = self.heap_array[popped_idx], self.heap_array[left_child_idx]
+                    popped_idx = left_child_idx
+            else:
+                if self.heap_array[left_child_idx] > self.heap_array[right_child_idx]:
+                    if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                        self.heap_array[left_child_idx], self.heap_array[popped_idx] = self.heap_array[popped_idx], self.heap_array[left_child_idx]
+                        popped_idx = left_child_idx                
+                else:
+                    if self.heap_array[right_child_idx] > self.heap_array[popped_idx]:
+                        self.heap_array[right_child_idx], self.heap_array[popped_idx] = self.heap_array[popped_idx], self.heap_array[right_child_idx]
+                        popped_idx = right_child_idx
+                        
+
+        return returned_data
+                
+    
+    def move_down(self, popped_idx):
+        left_child_idx = popped_idx * 2
+        right_child_idx = popped_idx * 2 + 1
+
+        if left_child_idx >= len(self.heap_array):
+            return False
+        elif right_child_idx >= len(self.heap_array):
+            if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                return True
+            else:
+                return False
+        else:
+            if self.heap_array[left_child_idx] > self.heap_array[right_child_idx]:
+                if self.heap_array[left_child_idx] > self.heap_array[popped_idx]:
+                    return True
+                else:
+                    return False
+            else:
+                if self.heap_array[right_child_idx] > self.heap_array[popped_idx]:
+                    return True
+                else:
+                    return False
+heap = Heap(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
+print(heap.heap_array)
+
+a = heap.pop()
+print(a)
+
+print(heap.heap_array)
