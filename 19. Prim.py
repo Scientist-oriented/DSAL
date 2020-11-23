3c2f5f1cfc86aaaa88d1baf1bdf9d9c76df721a1
Scientist-oriented <moon910904@gmail.com>
2020-11-23 10:55:34 +0900

    개선된 프림알고리즘 구현

--- 19. Prim.py
+++ 19. Prim.py
@@ -38,3 +38,52 @@ def prim(start_node, edges):
 
 a = prim('A', myedges)
 print(a)
+
+# 개선된 프림알고리즘 구현
+
+mygraph = {
+    'A': {'B': 7, 'D': 5},
+    'B': {'A': 7, 'D': 9, 'C': 8, 'E': 7},
+    'C': {'B': 8, 'E': 5},
+    'D': {'A': 5, 'B': 9, 'E': 7, 'F': 6},
+    'E': {'B': 7, 'C': 5, 'D': 7, 'F': 8, 'G': 9},
+    'F': {'D': 6, 'E': 8, 'G': 11},
+    'G': {'E': 9, 'F': 11}    
+}
+
+from heapdict import heapdict
+
+def imp_prim(graph, start):
+    mst, keys, pi, total_weight = list(), heapdict(), dict(), 0
+
+    for node in graph.keys():
+        keys[node] = float('inf')
+        pi[node] = None
+
+    keys[start], pi[start] = 0, start
+
+    while keys:
+        current_node, current_key = keys.popitem()
+        mst.append([pi[current_node], current_node, current_key])
+        total_weight += current_key
+
+        for adjacent, weight in mygraph[current_node].items():
+            if adjacent in keys and weight < keys[adjacent]:
+                keys[adjacent] = weight
+                pi[adjacent] = current_node
+
+    return mst, total_weight
+
+mygraph = {
+    'A': {'B': 7, 'D': 5},
+    'B': {'A': 7, 'D': 9, 'C': 8, 'E': 7},
+    'C': {'B': 8, 'E': 5},
+    'D': {'A': 5, 'B': 9, 'E': 7, 'F': 6},
+    'E': {'B': 7, 'C': 5, 'D': 7, 'F': 8, 'G': 9},
+    'F': {'D': 6, 'E': 8, 'G': 11},
+    'G': {'E': 9, 'F': 11}    
+}
+mst, total_weight = imp_prim(mygraph, 'A')
+print ('MST:', mst)
+print ('Total Weight:', total_weight)
+
