803f7c208dd1705533d2a5d0313151a17cfe2eca
Scientist-oriented <moon910904@gmail.com>
2020-12-17 22:00:24 +0900

    12월 17일 알고리즘 문제풀이

--- 21. Test_borad.py
+++ 21. Test_borad.py
@@ -1,15 +1,2 @@
-import heapq
-
-heap = []
-
-heapq.heappush(heap, 1)
-heapq.heappush(heap, 6)
-heapq.heappush(heap, 2)
-heapq.heappush(heap, 4)
-heapq.heappush(heap, 7)
-
-print(heapq.heappop(heap))
-print(heapq.heappop(heap))
-print(heapq.heappop(heap))
-print(heapq.heappop(heap))
-print(heapq.heappop(heap))
+array = input().split()
+print(array)
--- Q18.py
+++ Q18.py
@@ -27,3 +27,40 @@ dfs(0)
 print(result)
 
 
+# 알파벳 문제
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+    # 좌표의 이동
+    # 각 인덱스별로 좌, 우, 하, 상으로 이동
+
+def bfs(x, y):
+    global result
+    q = set()
+    # 중복은 1회만 계산하기 위해서 set 사용
+    q.add((x, y, array[x][y]))
+
+    while q:
+        x, y, step = q.pop()
+        # 스텝에는 지금까지 지나온 알파벳이 str형태로 들어있음.
+        result = max(result, len(step))
+        # result step의 길이와 비교해서 계속 업데이트 해나감
+
+        for i in range(4):
+            # 상하좌우 모든 방향을 고려
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if (0 <= nx and nx < r and 0 <= ny and ny < c and array[nx][ny] not in step):
+                # 이동한 점이 보드를 벗어나지 않고 지금까지 지나온 알파벳이 아닌 경우
+                q.add((nx, ny, step + array[nx][ny]))
+
+r, c = map(int, input().split())
+array = []
+for _ in range(r):
+    array.append(input())
+    # str와 리스트로 이루어진 2차원 리스트로 array를 구성
+
+result = 0
+bfs(0, 0)
+print(result)
