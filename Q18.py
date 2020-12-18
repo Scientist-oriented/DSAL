0ea09553297e2ab1e8d7d7e7bbed622a4ee3af29
Scientist-oriented <moon910904@gmail.com>
2020-12-18 16:26:55 +0900

    12월 18일 알고리즘 문제풀이

--- Q18.py
+++ Q18.py
@@ -64,3 +64,77 @@ for _ in range(r):
 result = 0
 bfs(0, 0)
 print(result)
+
+# 암호 만들기 문제
+
+from itertools import combinations
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+l, c = map(int, input().split(' '))
+
+array = input().split(' ')
+    # 여러 개가 들어오면 기본적으로 리스트로 저장
+array.sort()
+    # 사전식으로 출력해야 하므로 정렬을 수행
+
+# 길이가 l인 모든 암호 조합을 확인
+for password in combinations(array, l):
+    count = 0
+    for i in password:
+        if i in vowels:
+            count += 1
+    # password 안에 있는 모음의 개수를 센다.
+
+    if count >= 1 and count <= l - 2:
+        # 최소한 한개의 모음이 있는지
+        # 그리고 l -2보다는 적게 모음이 있는지 (= 2개 이상의 자음이 있는지)
+        print(''.join(password))
+        # 리스트를 문자열로 합쳐주는 역할
+
+# 암호 만들기 문제 (combination 함수 직접 만들기)
+
+import copy
+
+result = []
+string = []
+visited = []
+
+def combination(array, length, index):
+    if len(string) == length:
+        result.append(copy.deepcopy(string))
+        return
+    # 각 원소를 한 번씩만 뽑도록 구성
+    for i in range(index, len(array)):
+        if i in visited:
+            continue
+        string.append(array[i])
+        visited.append(i)
+        combination(array, length, i + 1)
+        string.pop()
+        visited.pop()
+
+    # array = [0, 1, 2, 3, 4]
+    # combination(array, 2, 0)으로 예를 들면
+    # 일단 실행을 시키면 0번 인덱스로 시작하는 조합이 구해지기 위해서
+        # combination(array, 2, 1)들이 돌기 시작함.
+        # str은 [0]인 상태
+    # 다시 1번 인엑스로 시작하는 조합이 구해지기 위해서
+        # combination(array, 2, 2)들이 돌기 시작함.
+        # str는 [1]인 상태
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+l, c = map(int, input().split(' '))
+
+array = input().split()
+array.sort()
+
+combination(array, 1, 0)
+
+for password in result:
+    count = 0
+    for i in password:
+        if i in vowels:
+            count += 1
+
+    if count >= 1 and count <= l - 2:
+        print(''.join(password))
