5045c5091e8f87a7f312f767c52ff6dc3ac5fe7d
Scientist-oriented <moon910904@gmail.com>
2020-12-20 20:50:08 +0900

    12월 20일 알고리즘 복습

---
+++ Q_copy.py
@@ -0,0 +1,128 @@
+# 음계 문제
+
+a = list(map(int, input().split(' ')))
+
+ascending = True
+descending = True
+
+for i in range(1, 8):
+    if a[i] > a[i - 1]:
+        descending = False
+    elif a[i] < a[i - 1]:
+        ascending = False
+
+if ascending:
+    print('ascending')
+elif descending:
+    print('descending')
+else:
+    print('mixed')
+
+# 블랙잭
+
+n, m = list(map(int, input().split(' ')))
+data = list(map(int, input().split(' ')))
+
+result = 0
+length = len(data)
+
+count = 0
+
+for i in range(0, length):
+    for j in range(i + 1, length):
+        for k in range(j + 1, length):
+            sum_value = data[i] + data[j] + data[k]
+            if sum_value <= m:
+                result = max(result, sum_value)
+
+print(result)
+
+# 스택수열
+
+n = int(input())
+
+count = 1
+stack = []
+result = []
+
+for i in range(1, n + 1):
+    data = int(input())
+    while count <= data:
+        stack.append(count)
+        count += 1
+        result.append('+')
+    if stack[-1] == data:
+        stack.pop()
+        result.append('-')
+    else:
+        print('NO')
+        exit(0)
+
+print('\n'.join(result))
+
+# 프린터 큐
+
+test_case = int(input())
+
+for _ in range(test_case):
+    n, m = list(map(int, input().split(' ')))
+    queue = list(map(int, input().split(' ')))
+    queue = [(i, idx) for idx, i in enumerate(queue)]
+
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == m:
+                print(count)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
+
+# 키로거 문제
+
+test_case = int(input())
+
+for _ in range(test_case):
+    left_stack = []
+    right_stack = []
+    data = input()
+    for i in data:
+        if i == '-':
+            if left_stack:
+                left_stack.pop()
+        elif i == '<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i == '>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
+        else:
+            left_stack.append(i)
+    left_stack.extend(reversed(right_stack))
+    print(''.join(left_stack))
+
+# SHA-256
+
+import hashlib
+
+input_data = input()
+encoded_data = input_data.encode()
+result = hashlib.sha256(encoded_data).hexdigest()
+print(result)
+
+# 수 찾기
+
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i not in array:
+        print('0')
+    else:
+        print('1')
+
